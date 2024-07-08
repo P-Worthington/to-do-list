f220f4e955697df45b4a35a2b378c6738c5a05f8
Paul Worthington <pworthington17@gmail.com>
2024-07-08 09:48:06 +0000

    add id

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,10 +11,17 @@ function addToList(a) {
     const textnode = document.createTextNode(string);
     node.appendChild(textnode);
     document.getElementById("list").appendChild(node);
+    length = addID();
+    node.id = `list-id-${length}`;
+};
 
+function addID () {
+    let listItems = document.getElementsByClassName("list-item");
+    let length = listItems.length;
+    return length
 }
 
 function addNewItem() {
     let item = getValue(0)
     addToList(item)
-}
+};
